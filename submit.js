3e8efab4458fc9e13a7246ed18fe9deb2495417c
Anik <anik.khondokar09@gmail.com>
2025-08-04 19:47:46 +0600

    several problem solved

--- submit.js
+++ submit.js
@@ -3,3 +3,40 @@ var area = 2060;
 //write your code here
 console.log(area/2);
 
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 9999;
+//write your code here
+if (money >= 25000) {
+    console.log('Laptop');
+} else if (money >= 10000) {
+    console.log('Cycle');
+} else {
+    console.log('Chocolate');
+}
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 30 ;
+//write your code here
+
+for(let i=1; i<=lastDay; i++){
+    if(i%3==0){
+        console.log(i+' - '+'medicine');
+    }else{
+        console.log(i+' - '+'rest');
+    }
+}
+
+/** Problem 04 - (Delete / Store) */
+var fileName = "slipdf.txt";
+//write your code here
+if (fileName.startsWith("#") || fileName.endsWith(".pdf") || fileName.endsWith(".docx")) {
+    console.log("Store");
+} else {
+    console.log("Delete");
+}
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "mewo" , roll: 96 ,department: "cse" };
+//write your code here
+console.log(student.name+student.roll+"."+student.department+"@ph.ac.bd");
+
